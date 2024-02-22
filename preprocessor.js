f564dbeb40ef60bfb8fa2d5155f5a413e7aa6ab9
bkkmw <roboticts49@gmail.com>
2024-02-22 16:15:47 +0900

    feat: generate GPT request with multiple inputs

--- preprocessor.js
+++ preprocessor.js
@@ -1,31 +1,53 @@
 import fs from 'fs';
 import { saveFile } from './utils/file_ctrl.js';
-import getGptResponse from './utils/gpt_classifier.js';
+import classifyFoods from './utils/gpt_classifier.js';
 
 const FILE_PATH = 'output/crawl/details.json';
 const OUTPUT_DIR = 'output/gpt';
 const OUTPUT_FILE_NAME = 'gpt.json';
+const LOG_FILE_NAME = `failure_log.json`
+const STRIDE = 20;
 
 const jsonFile = fs.readFileSync(FILE_PATH, 'UTF-8');
-const jsonData = JSON.parse(jsonFile).slice(0, 5);
-// const jsonData = JSON.parse(jsonFile);
+// for test
+// const jsonData = JSON.parse(jsonFile).slice(0, 200);
+const jsonData = JSON.parse(jsonFile);
 
 console.log(`operation starts for ${jsonData.length} items`);
+let failed = [];
 
-// TODO: Add delay
-for(let i=0; i<jsonData.length; i++) {
-    const elem = jsonData[i];
-    if(elem.food === '') continue;
+for(let i=0; i<jsonData.length; i+=STRIDE) {
+    // End of Data
+    const end = i+STRIDE > jsonData.length ? jsonData.length : i+STRIDE;
 
-    const category = await getGptResponse(elem.food);
-    console.log(category);
-    
-    if(i % 5 === 0) {
-        console.log(`[${i}/${jsonData.length}] DONE`);
+    // assign food lists as array
+    let foodList = [];
+    for(let j=i; j<end; j++) {
+        foodList.push(jsonData[j].food === '' ? '한식' : jsonData[j].food.replaceAll('..|...', ''));
     }
-    elem.foodTags = category.split(',');
+    // console.log("input", foodList.length);
+
+    // classify
+    const foodTagList = await classifyFoods(foodList);
+    // console.log("res", foodTagList.length);
+
+    // Error occurred
+    if(foodList.length !== foodTagList.length) {
+        console.log(`[${i} - ${end}] failed. generated ${foodTagList.length} items for ${foodList.length}`);
+        failed.push(i);
+        continue;
+    }
+    
+    // response as JS object
+    for(let j=0; j<foodTagList.length; j++) {
+        jsonData[i+j].foodTags = foodTagList[j].split(':')[1].split(',');
+    }
+    
+    if(end % 100 === 0) console.log(`[${end}/${jsonData.length}] DONE`);
 }
 
-console.log(jsonData);
+console.log(`Finished : total ${jsonData.length} items, ${failed.length * STRIDE} items failed`);
 
+// save data
 saveFile(OUTPUT_DIR, OUTPUT_FILE_NAME, jsonData);
+saveFile(OUTPUT_DIR, LOG_FILE_NAME, failed);
--- utils/gpt_classifier.js
+++ utils/gpt_classifier.js
@@ -12,28 +12,70 @@ const FOOD_CATEGORIES = [
     '견과류 및 마른안주', '찜 및 탕'
 ];
 
-const SYSTEM_MESSAGE = 'Your job is to classify given sentense to given categories of food in korean.\
-Input message might be food, foods, or description about foods.\
-Result does not have to be single value, align all the possible categories seperated by comma.\
-Any other descriptions are not required. Simply answer possible categories in given categories.\
-Categories are' + FOOD_CATEGORIES.join([', ']);
+const SYSTEM_MESSAGE = 'Your job is to classify given input to target categories.\
+Input message might be food, foods, description about foods, or description about foods that well-matched to korean traditional alcohol\
+Result does not have to be single value, align all the possible categories separated by comma.\
+Any other descriptions are not required. Simply answer possible categories in given categories in format [input_number] : ouput\
+Target Categories are' + FOOD_CATEGORIES.join([', ']) + '.\
+There might be multiple inputs, generate outputs for each input.\
+And your output message for each input must be also separated by new line character.\
+None of inputs can be omitted, the accuracy of answer is less important than output format. Do not ommit any inputs\
+';
 
 /**
  * generate chat GPT completion response that classifies categories of food.
  * @param {*} msg target message to classify
- * @return string : comma seperated categories
+ * @return {Promise<String>} : comma seperated categories
  */
-async function getGptResponse(msg) {
+async function getGptResponseMessage(msg) {
     const completions = await openai.chat.completions.create({
         messages: [
             {"role": "system", "content": SYSTEM_MESSAGE},
             {"role": "user", "content": msg}
         ],
         model: "gpt-3.5-turbo",
-        temperature: 1.2,
+        temperature: 1.0,
     })
 
     return completions.choices[0].message.content;
 };
 
-export default getGptResponse;
+/**
+ * generate chat GPT completion response that classifies categoreis of multiple food list.
+ * @param {*} prompts target prompts(messages) to classify - { "role", "content" }
+ * @returns {Promise<String>} result : new line separated lists and comma separated categories
+ */
+async function getGptResponseMessages(prompts) {
+    // console.log(prompts)
+    const completions = await openai.chat.completions.create({
+        messages: [
+            {"role": "system", "content": SYSTEM_MESSAGE},
+            ...prompts
+        ],
+        model: "gpt-3.5-turbo",
+        temperature: 0.8,
+    })
+
+    return completions.choices[0].message.content;
+};
+
+/**
+ * Classify food categories by Chat GPT
+ * @param {Array} foodList list of foods
+ * @returns {Promise<String[]>} list of categories
+ */
+async function classifyFoods(foodList) {
+    const prompts = [];
+    for(let i=0; i<foodList.length; i++) {
+        const prompt = {"role": "user", "content": `input ${i+1}: ${foodList[i]}`};
+
+        prompts.push(prompt);
+    }
+
+    const res = await getGptResponseMessages(prompts);
+    // console.log(res);
+
+    return res.split('\n');
+}
+
+export default classifyFoods;
